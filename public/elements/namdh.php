dd880863be3c8bc38e841a3eed16d27ed39cb35a
namdhgc <namdhgc@gmail.com>
2019-02-22 15:15:29 +0700

    fix jquery library

--- public/elements/namdh.php
+++ public/elements/namdh.php
@@ -101,7 +101,8 @@
 
     <!-- Load JS here for greater good =============================-->
     <!-- <script src="{{ URL::to('src/js/vendor/jquery.min.js') }}"></script> -->
-    <script src="src/js/vendor/jquery.min.js"></script>
+    <!-- <script src="js/vendor/jquery.min.js"></script> -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="js/build/build.min.js"></script>
     <script type="text/javascript">
         $(document).ready(function(){
